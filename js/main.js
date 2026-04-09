d08ff3c951b4e0eba4c9a73c8bbbbe94c6ef3898
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2026-04-09 17:47:33 +0400

    branded-stoves

--- js/main.js
+++ js/main.js
@@ -1,24 +1,41 @@
 $(document).ready(function () {
-  $('.about-info__item.active .about-info__body').each(function () {
-    $(this).css('max-height', this.scrollHeight + 'px');
+  $(".about-info__item.active .about-info__body").each(function () {
+    $(this).css("max-height", this.scrollHeight + "px");
   });
 
-  $('.about-info__head').on('click', function () {
-    const $item = $(this).closest('.about-info__item');
-    const $body = $item.find('.about-info__body');
-    const isActive = $item.hasClass('active');
+  $(".about-info__head").on("click", function () {
+    const $item = $(this).closest(".about-info__item");
+    const $body = $item.find(".about-info__body");
+    const isActive = $item.hasClass("active");
 
     if (isActive) {
-      $body.css('max-height', 0);
-      $item.removeClass('active');
+      $body.css("max-height", 0);
+      $item.removeClass("active");
     } else {
-      $('.about-info__item.active').each(function () {
-        $(this).removeClass('active');
-        $(this).find('.about-info__body').css('max-height', 0);
+      $(".about-info__item.active").each(function () {
+        $(this).removeClass("active");
+        $(this).find(".about-info__body").css("max-height", 0);
       });
 
-      $item.addClass('active');
-      $body.css('max-height', $body[0].scrollHeight + 'px');
+      $item.addClass("active");
+      $body.css("max-height", $body[0].scrollHeight + "px");
     }
+  });
+
+  $(".hotspot__dot").on("click", function (e) {
+    e.stopPropagation();
+
+    const $hotspot = $(this).closest(".hotspot");
+    const isActive = $hotspot.hasClass("active");
+
+    $(".hotspot").removeClass("active");
+
+    if (!isActive) {
+      $hotspot.addClass("active");
+    }
+  });
+
+  $(document).on("click", function () {
+    $(".hotspot").removeClass("active");
   });
 });
